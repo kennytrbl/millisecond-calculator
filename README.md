3f8826a1ba00c10b95ef408e94907a01a607c9b6
kennytrbl <kennyzhang418@gmail.com>
2025-02-06 11:31:20 -0800

    final commit

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # millisecond-calculator
+
+[Live Demo](https://ms-calculator.kennyzhang.dev/)
+
 A website for converting milliseconds into various time units.
 
-https://millisecond-calculator.vercel.app/
+## Tech Stack
+
+React, Javascript, Vercel
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { sendToVercelAnalytics } from "./vitals";
 import Footer from "./Footer";
 
 ReactDOM.render(
@@ -11,7 +9,5 @@ ReactDOM.render(
     <App />
     <Footer />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
-
-reportWebVitals(sendToVercelAnalytics);
